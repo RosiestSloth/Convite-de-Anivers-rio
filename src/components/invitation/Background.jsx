db661e5ac8761b0105e30b7fab719cff4cbd658f
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-02-25 16:43:00 -0300

    Update Background.jsx

--- src/components/invitation/Background.jsx
+++ src/components/invitation/Background.jsx
@@ -3,7 +3,11 @@ import { particles } from "./constants";
 export function Background() {
   return (
     <>
-      <div id="fundo-site" className="fixed inset-0 z-0 bg-cover bg-center bg-[url('img/ocean_bg.png')]">
+      <div
+        id="fundo-site"
+        className="fixed inset-0 z-0 bg-cover bg-center"
+        style={{ backgroundImage: `url('${import.meta.env.BASE_URL}img/ocean_bg.png')` }}
+      >
         <div className="inset-0 absolute bg-linear-to-tl from-(--color-cobalt)/70 via-(--color-ocean-deep)/60 to-transparent" />
       </div>
 
